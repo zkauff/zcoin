003195509d9c8d4af561e9dac244d8033a110a54
Zachary Kauffman <ztk@iastate.edu>
2021-10-09 16:35:15 -0500

    Refactored code to use "transactions" over "data"

--- Block.py
+++ Block.py
@@ -9,12 +9,12 @@ class Block(object):
                  idx, 
                  proof_num,
                  prev_hash,
-                 data,
+                 transactions,
                  timestamp=time.time()):
         self.idx = idx
         self.proof_num = proof_num
         self.prev_hash = prev_hash
-        self.data = data
+        self.transactions = transactions
         self.timestamp = timestamp
         self.nonce = 2
 
@@ -22,24 +22,34 @@ class Block(object):
         self.nonce = nonce
 
     def print(self):
+        """
+        Prints out the current block in a readable format.
+        :return: <str>
+        """
         block_str = "----------\nBlock ID: {}\nProof #{}\nHash: {}\nPrev Hash: {}\nData: {}\nTimestamp: {}".format(
             self.idx,
             self.proof_num,
             self.compute_hash,
             self.prev_hash,
-            self.data,
+            self.transactions,
             self.timestamp
             )
         print(block_str)
+        return block_str
 
     @property
     def compute_hash(self):
+        """
+        Creates a SHA-256 hash of this block.
+        :return: <str>
+        """
+
         block_str = "{}{}{}{}{}{}".format(
             self.idx,
             self.nonce,
             self.proof_num,
             self.prev_hash,
-            self.data,
+            self.transactions,
             self.timestamp
             )
         return hashlib.sha256(block_str.encode()).hexdigest()
--- BlockChain.py
+++ BlockChain.py
@@ -8,10 +8,12 @@ Blockchain implementation using the Block class from Block.py as our
 links in the chain.
 """
 class BlockChain(object):
-    def __init__(self):
+    def __init__(self, difficulty=DIFFICULTY):
+        self.pow_difficulty = difficulty
         self.chain = []
-        self.curr_data = []
         self.nodes = set()
+        # The list of transactions that will go into the next block. 
+        self.current_transactions = [] 
         self.build_genesis()
         pass
 
@@ -25,15 +27,15 @@ class BlockChain(object):
 
     def build_block(self, proof_num, prev_hash, data=None):
         if not data:
-            data = self.curr_data
+            data = self.current_transactions
         block = Block(
             idx = len(self.chain),
             proof_num = proof_num,
             prev_hash = prev_hash,
-            data = data
+            transactions = data
         )
-        self.proof_of_work(proof_num, block, DIFFICULTY)
-        self.curr_data = data
+        self.proof_of_work(proof_num, block, self.pow_difficulty)
+        self.current_transactions = []
         self.chain.append(block)
         return block
 
@@ -43,24 +45,46 @@ class BlockChain(object):
             prev_block.idx + 1 != block.idx
             or prev_block.compute_hash != block.prev_hash
             or prev_block.timestamp >= block.timestamp
+            or not util.verify_hash_zeroes(block.compute_hash, DIFFICULTY)
             ):
             return False
         else:
             return True
 
     def get_data(self, sender, receiver, amount):
-        self.curr_data = transaction(sender, receiver, amount).json()
-        return True
+        """
+        Creates a new transaction to go into the Block successfully mined. 
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append(transaction(sender, receiver, amount).json())
+        return len(self.chain) + 1
 
     @staticmethod
     def proof_of_work(prev_proof, block, difficulty):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a nonce such that block.compute_hash() contains leading self.difficulty zeroes,
+         where the block.compute_hash() is an encoding of 
+            block.idx,
+            block.nonce
+            block.proof_num,
+            block.prev_hash,
+            block.transactions, 
+            block.timestampe
+        :param prev_proof: <int>
+        :param block: <Block>
+        :param difficulty: <int>
+        :return: <int>
+        """
         i = 0
         # Try to find a nonce such that the block's hash has `difficulty` zeros.
         while True:
             block.set_nonce(i)
             h = block.compute_hash
             if util.verify_hash_zeroes(h, difficulty):
-                print(h)
                 return prev_proof + 1
             i = i + 1
 
@@ -76,7 +100,7 @@ class BlockChain(object):
         )
         last_block = self.latest_block
         block = self.build_block(last_block.proof_num + 1, last_block.compute_hash)
-        self.proof_of_work(last_block.proof_num, block, DIFFICULTY)
+        self.proof_of_work(last_block.proof_num, block, self.pow_difficulty)
         return vars(block)
 
     def create_node(self, address):
@@ -94,9 +118,9 @@ class BlockChain(object):
         )
 
 def main():
-    blockchain = BlockChain()
+    blockchain = BlockChain(difficulty=DIFFICULTY)
     last_block = blockchain.latest_block
-    proof_num = blockchain.proof_of_work(last_block.proof_num, last_block, DIFFICULTY)
+    proof_num = blockchain.proof_of_work(last_block.proof_num, last_block, blockchain.pow_difficulty)
     blockchain.mine_block("galaxy")
     data = transaction("galaxy", "galaxy2", 50).json()
     last_hash = last_block.compute_hash
