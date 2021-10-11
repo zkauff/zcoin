fabea221f65ba6166d32b323acadbeb6c9255cfa
Zachary Kauffman <ztk@iastate.edu>
2021-10-11 18:47:48 -0500

    Added pytest fixtures

--- zcoin_client_test.py
+++ zcoin_client_test.py
@@ -1,14 +1,13 @@
 from zcoin_client import zcoin_instance
 import requests
 import threading
+import socket
+import pytest
 
-def test_consensus_update_chain():
-    """
-    Sets up two instances of the blockchain. One will not mine any coins,
-    and the other will mine one coin with some transactions.
-    The consensus algorithm will be run and the blockchain without any mined
-    coins should accept the other chain.
-    """
+ip = "0.0.0.0"
+
+@pytest.fixture
+def run_client_instances():
     good_chain = zcoin_instance(5000)
     empty_chain = zcoin_instance(5001)
     p1 = threading.Thread(target=good_chain.run, args=())
@@ -17,14 +16,24 @@ def test_consensus_update_chain():
     p2 = threading.Thread(target=empty_chain.run, args=())
     p2.daemon = True
     p2.start()
-    requests.get("http://0.0.0.0:5000/mine")
-    requests.get("http://0.0.0.0:5000/mine") 
-    original_chain = requests.get("http://0.0.0.0:5000/chain").json()
+    global ip
+    ip = socket.gethostbyname(socket.gethostname())
+
+def test_consensus_update_chain(run_client_instances):
+    """
+    Sets up two instances of the blockchain. One will not mine any coins,
+    and the other will mine one coin with some transactions.
+    The consensus algorithm will be run and the blockchain without any mined
+    coins should accept the other chain.
+    """
+    requests.get(f"http://{ip}:5000/mine")
+    requests.get(f"http://{ip}:5000/mine") 
+    original_chain = requests.get(f"http://{ip}:5000/chain").json()
     # add peer nodes
-    requests.post("http://0.0.0.0:5001/peers/add", json={"peers": ["0.0.0.0:5000"]} )
-    chain_before = requests.get("http://0.0.0.0:5001/chain").json()
-    requests.get("http://0.0.0.0:5001/peers/consensus")
-    chain_after = requests.get("http://0.0.0.0:5001/chain").json()
+    requests.post(f"http://{ip}:5001/peers/add", json={"peers": [f"{ip}:5000"]} )
+    chain_before = requests.get(f"http://{ip}:5001/chain").json()
+    requests.get(f"http://{ip}:5001/peers/consensus")
+    chain_after = requests.get(f"http://{ip}:5001/chain").json()
     print(chain_before)
     print(chain_after)
     assert chain_after != chain_before
